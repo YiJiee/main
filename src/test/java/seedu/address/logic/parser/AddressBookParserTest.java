d30001c9db13c2321e25f494ee2f3cc292c3e7d5
LimYiJie <e0325578@u.nus.edu>
2019-09-23 02:50:27 +0800

    Removed address field

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -36,7 +36,8 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_add() throws Exception {
         Person person = new PersonBuilder().build();
-        AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
+        String userInput = PersonUtil.getAddCommand(person) + "a/";
+        AddCommand command = (AddCommand) parser.parseCommand(userInput);
         assertEquals(new AddCommand(person), command);
     }
 
@@ -57,8 +58,9 @@ public class AddressBookParserTest {
     public void parseCommand_edit() throws Exception {
         Person person = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
-        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
+        String userInput = EditCommand.COMMAND_WORD + " "
+                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor);
+        EditCommand command = (EditCommand) parser.parseCommand(userInput);
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
